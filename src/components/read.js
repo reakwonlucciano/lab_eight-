7b3be62b3a52dd83de9736d9058452b11d47e2fb
Reakwon Lucciano <62098320+reakwonlucciano@users.noreply.github.com>
2022-12-04 22:10:24 +0000

    Update read.js

--- src/components/read.js
+++ src/components/read.js
@@ -1,30 +1,43 @@
 import React from "react";
-import { Books } from "./books";
+//Import Axios
 import axios from "axios";
 
+//Import Books
+import { Books } from "./books";
+
 export class Read extends React.Component{
-    
-
-    componentDidMount() {
-        axios.get('https://jsonblob.com/api/jsonblob/1027219693823606784')
-        .then((response)=>{
-            this.setState({books:response.data})
-        })
-        .catch((error)=>{
-            console.log(error);
-        })
+    componentDidMount(){
+        //Makes HTTP Request
+        axios.get('http://localhost:2000/api/books')
+        //When Request Completed
+        .then(
+            //New way of Functions
+            (response)=>{
+                // Update State
+                this.setState({books:response.data})
+            }
+        )
+        //If Request returns error
+        .catch(
+            (error)=>{
+                console.log(error)
+            }
+        );
     }
 
+    //Object that will hold all data for class
     state = {
-        books:[ ]
+        //Assign Data to Array
+        books: []
     }
-    
+
     render(){
         return(
             <div>
-                <h3>Hello from my Read component!</h3>
-                <Books books={this.state.books}></Books>
+                <h3>Hello from my Read Component!</h3>
+                {/* Display Book Class from Book Component & Pass in Variable */}
+                <Books books = {this.state.books}></Books>
             </div>
-        );
+        )
     }
 }
