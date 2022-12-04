d1a6f1e2801230d9b7d4dc19cabda5b5918864a4
Reakwon Lucciano <62098320+reakwonlucciano@users.noreply.github.com>
2022-12-04 22:11:07 +0000

    Update bookItem.js

--- src/components/bookItem.js
+++ src/components/bookItem.js
@@ -1,26 +1,28 @@
+//Imports
 import React from "react";
 import Card from 'react-bootstrap/Card';
+import { Link } from "react-router-dom";
 
-export class BookItem extends React.Component {
-    render() {
-        return (
-            <div>
-                {/* <h4>{this.props.book.title}</h4>
-        <img src={this.props.book.thumbnailUrl}></img>
-                <h6>{this.props.book.authors[0]}</h6> */}
-
+export class BookItem extends React.Component{
+    render(){
+        return(
+            <div className="bookItem">
+                {/* Print out info from Array as Cards */}
                 <Card>
-            <Card.Header>{this.props.book.title}</Card.Header>
+                    {/* Book Title */}
+                    <Card.Header>{this.props.book.title}</Card.Header>
                     <Card.Body>
-       <blockquote className="blockquote mb-0">
-            <img src={this.props.book.thumbnailUrl}></img>
-            <footer >
-                {this.props.book.authors[0]}
-                    </footer>
+                        {/* Show Picture and author to screen */}
+                        <blockquote className = "blockqoute mb-0">
+                            <img src = {this.props.book.thumbnailUrl} width = "150" height = "150"></img>
+                            <footer>
+                                {this.props.book.authors[0]}
+                            </footer>
                         </blockquote>
                     </Card.Body>
-                </Card>
-            </div>
-        );
+                     <Link to={'/edit/'+this.props.book._id} className="btn btn-primary">Edit</Link>
+            </Card>
+          </div>
+        )
     }
 }
